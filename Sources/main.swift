1bd6c9eff25a053f7054047b2eb9261351048dbf
tomieq <tomieq@wp.pl>
2025-06-09 11:05:52 +0200

    Removed redundant sample code

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,21 +10,3 @@ try server.start(8080)
 
 logger.i("Server started on port \(try server.port)")
 RunLoop.main.run()
-
-
-if #available(macOS 10.15, *) {
-    Task {
-        do {
-            let url = URL(string: "http://localhost:8080/cache/ip")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "PUT"
-            request.httpBody = "data to store".data(using: .utf8)
-            let data = try await URLSession.shared.data(for: request)
-            print("received: \(String(data: data.0, encoding: .utf8).or(""))")
-        } catch {
-            print("Error: \(error)")
-        }
-    }
-} else {
-    // Fallback on earlier versions
-}
